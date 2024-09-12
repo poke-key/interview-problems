aa7b072345cf2dfafb0d452f0d599e92e79a6ca6
poke-key <kunalshrivastav345@gmail.com>
2024-09-11 23:05:29 -0700

    added test cases

--- matrixZero.cpp
+++ matrixZero.cpp
@@ -11,13 +11,35 @@ void setMatrixToZero(vector<vector<int>>& matrix) {
     int m = matrix.size(); //get size of matrix
     int n = matrix[0].size(); //get size of beginning
 
+    for(int i = 0; i < m; i++) {
+        for(int j = 0; j < n; j++) {
+
+            matrix[i][j] = 0; //set as 0 instead
+        }
+    }
 
 }
 
 //test case over here
 int main() {
 
+    vector<vector<int>> matrix = {
+        {1, 2, 3}, 
+        {4, 5, 6}, 
+        {7, 8, 9}
+    };
 
+    setMatrixToZero(matrix);
+
+    for(auto row : matrix) {
+        for(auto elem : matrix) {
+            cout << elem << " ";
+        }
+
+        cout << endl;
+    } 
+
+    return 0;
 
 
 }
