3ef0c4dfb8cb0302a825d4696781e4e24c01b4f5
poke-key <kunalshrivastav345@gmail.com>
2024-09-12 01:08:47 -0700

    initialized hash map and data

---
+++ phoneBook.cpp
@@ -0,0 +1,27 @@
+//question: best data struct for phone books with names + numbers
+
+//answer: hashmap
+
+//coded example
+
+#include <iostream>
+#include <unordered_map>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+    unordered_map<string, string> phoneBook;
+
+    //add numbers + names
+    phoneBook["Kunal"] = "555-1234";
+    phoneBook["Tushar"] = "555-5678";
+    phoneBook["Samheart"] = "555-8765";
+    phoneBook["FalakPaneer"] = "555-2345";
+    phoneBook["RAM"] = "555-3288";
+
+    //look up number
+    
+
+}
