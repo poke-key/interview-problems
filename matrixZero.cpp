986ec304fdb550f0b8f7fe714d31d2fc2c924476
poke-key <kunalshrivastav345@gmail.com>
2024-09-11 22:57:07 -0700

    matrix to zero problem baseline

---
+++ matrixZero.cpp
@@ -0,0 +1,20 @@
+//problem: write a function to take in m x n matrix (array) of random numbers, 
+//and use the functions row (times) 2 and column-1 to set the entire array to 0
+
+#include <iostream> 
+#include <vector>
+using namespace std;
+
+//takes in 2d vector
+void setMatrixToZero(vector<vector<int>>& matrix) {
+
+
+
+}
+
+//test case over here
+int main() {
+
+
+
+}
